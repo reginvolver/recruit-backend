506a4c379ae7ca13bf1b657339195458298ec902
xiaowenzhuo <reginvolver@gmail.com>
2023-07-14 01:03:45 +0800

    增加apollo动态配置

--- recruit-web/src/main/java/com/yundingshuyuan/recruit/RecruitApplication.java
+++ recruit-web/src/main/java/com/yundingshuyuan/recruit/RecruitApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecruitApplication {
     public static void main(String[] args) {
+        System.setProperty("apollo.config-service", "http://localhost:8080");
         SpringApplication.run(RecruitApplication.class, args);
     }
 }
--- recruit-web/src/main/java/com/yundingshuyuan/recruit/web/controller/TestController.java
+++ recruit-web/src/main/java/com/yundingshuyuan/recruit/web/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.yundingshuyuan.recruit.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test}")
+    private String test;
+
     @GetMapping("/test")
     String test() {
-        return "test";
+        return test;
     }
 }
