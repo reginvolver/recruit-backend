10707798d04f8186785b9c58c5ae10b78078c477
xiaowenzhuo <reginvolver@gmail.com>
2023-07-14 23:16:10 +0800

    增加redis支持

--- recruit-web/src/main/java/com/yundingshuyuan/recruit/web/controller/TestController.java
+++ recruit-web/src/main/java/com/yundingshuyuan/recruit/web/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.yundingshuyuan.recruit.web.controller;
 
+import com.yundingshuyuan.recruit.utils.RedisUtils;
 import com.yundingshuyuan.recruit.web.annotation.RecruitResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -7,6 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Tag(name = "测试接口")
 @RecruitResult
+@Slf4j
 public class TestController {
+
+    @Autowired
+    private RedisUtils redisUtils;
 
     @Value("${test}")
     private String test;
@@ -22,6 +29,8 @@ public class TestController {
     @GetMapping("/test")
     @Operation(summary = "测试接口")
     public User test() {
+        boolean redisTest = redisUtils.set("test", test);
+        log.info("redisTest:{}", redisTest);
         return new User("张三", test);
     }
 
