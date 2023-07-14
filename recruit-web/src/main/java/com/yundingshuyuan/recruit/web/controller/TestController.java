20deb4e5c48dbb95d158694722385bd52cda9b7e
xiaowenzhuo <reginvolver@gmail.com>
2023-07-14 21:16:36 +0800

    增加knife4j依赖

--- recruit-web/src/main/java/com/yundingshuyuan/recruit/web/controller/TestController.java
+++ recruit-web/src/main/java/com/yundingshuyuan/recruit/web/controller/TestController.java
@@ -1,17 +1,22 @@
 package com.yundingshuyuan.recruit.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "测试接口")
 public class TestController {
 
     @Value("${test}")
     private String test;
 
     @GetMapping("/test")
+    @Operation(summary = "测试接口")
     String test() {
         return test;
     }
+
 }
