2a5eff045e7b6e583a59603ff362438c0c2683ba
lipengxun <450582885@qq.com>
2023-08-07 09:57:58 +0800

    完善功能

--- recruit-support/src/main/java/com/yundingshuyuan/recruit/dao/RegisterInfoMapper.java
+++ recruit-support/src/main/java/com/yundingshuyuan/recruit/dao/RegisterInfoMapper.java
@@ -1,26 +1,11 @@
 package com.yundingshuyuan.recruit.dao;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.yundingshuyuan.recruit.domain.DirectionCountVO;
 import com.yundingshuyuan.recruit.domain.RegisterInfo;
+import com.yundingshuyuan.recruit.domain.vo.RegisterInfoVO;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 @Mapper
-public interface RegisterInfoMapper extends BaseMapper<RegisterInfo> {
-    @Select("SELECT register_info.direction,\n" +
-            "       COUNT(*) AS total,\n" +
-            "       SUM(CASE WHEN DATE(register_info.create_time) = CURDATE() THEN 1 ELSE 0 END) AS today,\n" +
-            "       SUM(CASE WHEN user_info.gender = 1 THEN 1 ELSE 0 END) AS maleCount,\n" +
-            "       SUM(CASE WHEN user_info.gender = 0 THEN 1 ELSE 0 END) AS femaleCount\n" +
-            "FROM register_info\n" +
-            "JOIN user_info ON register_info.user_id = user_info.id\n" +
-            "GROUP BY register_info.direction")
-    List<DirectionCountVO> countByDirection();
+public interface RegisterInfoMapper extends BaseMapperPlus<RegisterInfo, RegisterInfoVO> {
 
-    @Select("SELECT COUNT(*) FROM register_info")
-    int countRegisterInfo();
 
 }
