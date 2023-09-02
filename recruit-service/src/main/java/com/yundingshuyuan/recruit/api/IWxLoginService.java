9e8b21599a5792e4365b00f9ec6661a0011debef
li-bee <2416355572@qq.com>
2023-09-02 16:51:12 +0800

    修改代码,去掉DisPlayController，增加CollegeController，大改

--- recruit-service/src/main/java/com/yundingshuyuan/recruit/api/IWxLoginService.java
+++ recruit-service/src/main/java/com/yundingshuyuan/recruit/api/IWxLoginService.java
@@ -3,6 +3,10 @@ package com.yundingshuyuan.recruit.api;
 import com.yundingshuyuan.vo.BasicResultVO;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.Map;
+
 public interface IWxLoginService {
-    public BasicResultVO<String> getOpenid(@RequestBody String data);
+    public String getOpenid(@RequestBody String data);
+
+    public Map<String,Integer> getLoginMes(String cloudId);
 }
--- recruit-web/src/main/java/com/yundingshuyuan/recruit/web/controller/WxLoginController.java
+++ recruit-web/src/main/java/com/yundingshuyuan/recruit/web/controller/WxLoginController.java
@@ -1,22 +1,38 @@
 package com.yundingshuyuan.recruit.web.controller;
 
+
 import com.yundingshuyuan.recruit.api.IWxLoginService;
+import com.yundingshuyuan.recruit.domain.LoginMes;
 import com.yundingshuyuan.vo.BasicResultVO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
+@Tag(name = "小程序登录接口")
 @Slf4j
 @RequestMapping("/wxlogin")
 public class WxLoginController {
+
     @Autowired
     public IWxLoginService wxLoginService;
 
-    @PostMapping("/getOpenid")
+    @Operation(summary = "获取用户登录时的openid,userid,权限")
+    @PostMapping("/getMessage")
     @ResponseBody
-    public BasicResultVO<String> getOpenid(@RequestBody String data) {
-        BasicResultVO result = wxLoginService.getOpenid(data);
-        return result;
+    public BasicResultVO<LoginMes> getMessage(@RequestBody String data) {
+        String openid = wxLoginService.getOpenid(data);
+        Map<String, Integer> mes = wxLoginService.getLoginMes(openid);
+        Integer id = mes.get("id");
+        Integer isAdmin = mes.get("isAdmin");
+
+        LoginMes loginMes = new LoginMes(openid,id,isAdmin);
+
+        return BasicResultVO.success(loginMes);
     }
+
 }
